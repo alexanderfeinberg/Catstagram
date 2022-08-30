2fc43d0365e6d2304ef5f8cfee459acf7aafe14c
Kyle Solano <kylegsolano@gmail.com>
2022-08-30 13:59:17 -0700

    complete phase 1

--- public/index.js
+++ public/index.js
@@ -1,1 +1,10 @@
 // Your code here
+window.onload = async () => {
+    const url = await fetch("https://api.thecatapi.com/v1/images/search")
+    const image = JSON.parse(await url.text())[0].url
+    console.log(image)
+
+    const newElement = document.createElement("img")
+    newElement.setAttribute("src", image)
+    document.body.append(newElement)
+}
